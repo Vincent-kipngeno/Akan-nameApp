0c9fd5cde6e3009c754dce33ff541b0ab094dc0b
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-02 09:56:19 +0300

    converts birthdate to string then declare centuryYear, birthYear, birthDay, birthMonth, year variables then assigns them values from the birthdate

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 function wawa() {
   var birthDate = document.getElementById('input_id').value;
   var gender = document.getElementById('gender').value;
+  birthDate.toString();
+  var centuryYear;
+  var birthYear;
+  var birthMonth;
+  var birthDay;
+  var year;
+  centuryYear = parseInt(birthDate.slice(0, 2));
+  birthYear = parseInt(birthDate.slice(2, 4));
+  birthMonth = parseInt(birthDate.slice(5, 7));
+  birthDay = parseInt(birthDate.slice(8, 10));
+  year = parseInt(birthDate.slice(0, 4));
 }
