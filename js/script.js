14ceb5f0477be1e5333bc937977a75d2e72ec61f
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-02 18:34:26 +0300

    Add if statement that alerts the user if they don't enter any date

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ function akan() {
   var birthDate = document.getElementById('date').value;
   var gender = document.getElementById('gender').value;
   birthDate.toString();
+  if (!birthDate) {
+    alert("You entered invalid date");
+  }
   var centuryYear;
   var birthYear;
   var birthMonth;
